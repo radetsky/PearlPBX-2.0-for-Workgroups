40cead60260ab4f1ba8230fb459910d709bd445f
Alex Radetsky <rad@spam.com.ua>
2016-04-08 22:16:35 +0300

    Update install.sh for database

--- database/scheme/install.sh
+++ database/scheme/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cp /etc/postgresql/9.4/main/pg_hba.conf /tmp/pg_hba.conf 
+cp /etc/postgresql/9.4/main/pg_hba.conf /tmp/pg_hba.conf
 cat /tmp/pg_hba.conf | sed 's/ident$/trust/' >/etc/postgresql/9.4/main/pg_hba.conf
 rm /tmp/pg_hba.conf
-cp  /etc/postgresql/9.4/main/pg_hba.conf /tmp/pg_hba.conf 
+cp  /etc/postgresql/9.4/main/pg_hba.conf /tmp/pg_hba.conf
 cat /tmp/pg_hba.conf | sed 's/peer$/trust/' >/etc/postgresql/9.4/main/pg_hba.conf
 systemctl restart postgresql
 psql -U postgres -f /etc/NetSDS/sql/create_user_asterisk.sql
 createdb -U postgres asterisk -O asterisk
-psql -U asterisk -f /etc/NetSDS/sql/postgresql_config.sql 
-psql -U asterisk -f /etc/NetSDS/sql/postgresql_cdr.sql 
-psql -U asterisk -f /etc/NetSDS/sql/postgresql_voicemail.sql 
-psql -U asterisk -f /etc/NetSDS/sql/pearlpbx.sql 
+psql -U asterisk -f /etc/NetSDS/sql/postgresql_config.sql
+psql -U asterisk -f /etc/NetSDS/sql/postgresql_cdr.sql
+psql -U asterisk -f /etc/NetSDS/sql/postgresql_voicemail.sql
+psql -U asterisk -f /etc/NetSDS/sql/pearlpbx.sql
 psql -U asterisk -f /etc/NetSDS/sql/callback.sql
 psql -U asterisk -f /etc/NetSDS/sql/directions_list.sql
 psql -U asterisk -f /etc/NetSDS/sql/directions.sql
